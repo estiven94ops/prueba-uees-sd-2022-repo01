46619b0f021c75bc23ef3738f0e7f1975010b90b
Steven Rodriguez <71090662+estiven94ops@users.noreply.github.com>
2022-01-22 18:19:13 -0500

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -5,16 +5,16 @@ public class TCPClient {
     public static final void main(final String[] args){
         Socket client;
         DataOutputStream dos;
-        InetAddress connection;
-        int random = (int)(Math.random()*10+1);
+        InetAddress jorge;
+        int numero = (int)(Math.random()*10+1);
 
         try{
-        connection = InetAddress.getByName("10.10.10.0");
-        client = new Socket(connection, 9999);
+        jorge = InetAddress.getByName("10.10.10.1");
+        client = new Socket(jorge, 9999);
 
         dos = new DataOutputStream(client.getOutputStream());
        
-        dos.write(random);
+        dos.write(numero);
 
         client.close();
 
