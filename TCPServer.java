f91493bb87292c289ba3c1ec0bf9a6d378670321
Steven Rodriguez <71090662+estiven94ops@users.noreply.github.com>
2022-01-22 18:20:01 -0500

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -1,35 +1,36 @@
-import java.io.InputStream;   import java.net.ServerSocket;   import java.net.Socket; import java.io.DataInputStream;
+import java.io.InputStream; import java.net.ServerSocket; import java.net.Socket; import java.io.DataInputStream;
 
 public class TCPServer {
-  public static final void main(final String[] args) {
-    ServerSocket server;           
-    InputStream  is;
+    public static final void main(final String[] args) {
+    ServerSocket server;
     Socket client;
+    InputStream is;
     DataInputStream dis;
     int n;
 
     try {
-      server = new ServerSocket(9999);//(*@\serverBox{1 + 2)}@*)
-      
-      for (int j = 5; (--j) >= 0;) {  //process only 5 clients, so I can show (*@\serverBox{5)}@*) below
-        client = server.accept(); //wait for incoming connection (*@\serverBox{3)}@*)
-        
-        System.out.println("New connection from " + client.getRemoteSocketAddress());
-        
-        dis     = new DataInputStream(client.getInputStream()); //get stream to read from
-        n = dis.read();//(*@\serverBox{4} + \clientBox{3})@*)
-        
-        for(int i = 0; i < n; i++){
-          System.out.print("X");
+        server = new ServerSocket(9999);
+
+        for (int j = 5; (--j) >= 0;){
+            client = server.accept();
+
+            dis = new DataInputStream(client.getInputStream());
+            n = dis.read();
+
+            for(int i =0; i<n; i++){
+                System.out.print("X");
+            }
+            System.out.print('\n');
+
+            client.close();
+            }
+        server.close();
+
+        } catch (Throwable t){
+            t.printStackTrace();
         }
-         System.out.print('\n');
-         System.out.print('\n');
-             
-        client.close(); //close connection to client
-      }
-      server.close();   //(*@\serverBox{5)}@*)
-    } catch (Throwable t) {
-      t.printStackTrace();
+
     }
-  }
+
+
 }
