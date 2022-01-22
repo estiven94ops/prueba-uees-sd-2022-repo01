0dc6a436f0f0c27ce6f4aa760afa50a6f3df15ba
Steven Rodriguez <71090662+estiven94ops@users.noreply.github.com>
2022-01-22 17:01:39 -0500

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -3,17 +3,18 @@ import java.io.OutputStream;  import java.net.InetAddress;  import java.net.Sock
 public class TCPClient {
 
   public static final void main(final String[] args) {
+    DataOutputStream os;
     Socket        client;
-    OutputStream  os;
     InetAddress   ia;
+    
+    int random = (int)(Math.random()*10+1);
 
     try {
-      ia = InetAddress.getByName("localhost");//get local host address
-      
+      ia = InetAddress.getByName("10.10.10.0");//get local host address
       client = new Socket(ia, 9999); //create socket (*@\clientBox{1+2)}@*)
       
-      os = client.getOutputStream(); //get stream to write to
-      os.write(1);  //write one byte of value 1 (*@\clientBox{3)}@*)
+      os = new DataOutputStream(client.getOutputStream()); //get stream to write to
+      cos.write(random);  //write one byte of value 1 (*@\clientBox{3)}@*)
       
       client.close(); //close (*@\clientBox{4)}@*)
     } catch (Throwable t) {
