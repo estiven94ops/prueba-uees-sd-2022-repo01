3733cd31203260ca59508c3adde280fc969c05f5
Steven Rodriguez <71090662+estiven94ops@users.noreply.github.com>
2022-01-22 17:49:15 -0500

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -1,24 +1,25 @@
-import java.io.OutputStream;  import java.net.InetAddress;  import java.net.Socket;
+import java.io.OutputStream; import java.net.InetAddress; import java.net.Socket; import java.util.Scanner; import java.io.DataOutputStream;
 
-public class TCPClient {
+public class TCPClientStructuredData {
 
-  public static final void main(final String[] args) {
-    DataOutputStream os;
-    Socket        client;
-    InetAddress   ia;
-    
-    int random = (int)(Math.random()*10+1);
+    public static final void main(final String[] args){
+        Socket client;
+        DataOutputStream dos;
+        InetAddress jorge;
+        int numero = (int)(Math.random()*10+1);
 
-    try {
-      ia = InetAddress.getByName("10.10.10.0");//get local host address
-      client = new Socket(ia, 9999); //create socket (*@\clientBox{1+2)}@*)
-      
-      os = new DataOutputStream(client.getOutputStream()); //get stream to write to
-      cos.write(random);  //write one byte of value 1 (*@\clientBox{3)}@*)
-      
-      client.close(); //close (*@\clientBox{4)}@*)
-    } catch (Throwable t) {
-      t.printStackTrace();
+        try{
+        jorge = InetAddress.getByName("10.10.10.0);
+        client = new Socket(jorge, 9999);
+
+        dos = new DataOutputStream(client.getOutputStream());
+       
+        dos.write(numero);
+
+        client.close();
+
+        } catch (Throwable t){
+        t.printStackTrace();
+        }
     }
-  }
 }
