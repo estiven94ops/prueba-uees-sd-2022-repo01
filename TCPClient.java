07876cac98da6b1675155fc7f6694ee449841cca
Steven Rodriguez <71090662+estiven94ops@users.noreply.github.com>
2022-01-22 17:50:47 -0500

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -5,16 +5,16 @@ public class TCPClientStructuredData {
     public static final void main(final String[] args){
         Socket client;
         DataOutputStream dos;
-        InetAddress jorge;
-        int numero = (int)(Math.random()*10+1);
+        InetAddress connection;
+        int random = (int)(Math.random()*10+1);
 
         try{
-        jorge = InetAddress.getByName("10.10.10.0);
-        client = new Socket(jorge, 9999);
+        connection = InetAddress.getByName("10.10.10.0);
+        client = new Socket(connection, 9999);
 
         dos = new DataOutputStream(client.getOutputStream());
        
-        dos.write(numero);
+        dos.write(random);
 
         client.close();
 
