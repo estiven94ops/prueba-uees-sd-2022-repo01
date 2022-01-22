7306a4d614d60b00e179dae57df83af7a9c3dbe0
Steven Rodriguez <71090662+estiven94ops@users.noreply.github.com>
2022-01-22 17:06:10 -0500

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -2,18 +2,27 @@ import java.io.InputStream;   import java.net.ServerSocket;   import java.net.So
 
 public class TCPServer {
   public static final void main(final String[] args) {
-    ServerSocket server;           InputStream  is;
-    Socket       client;          
+    ServerSocket server;           
+    InputStream  is;
+    Socket client;
+    DataInputStream dis;
+    int n;
 
     try {
       server = new ServerSocket(9999);//(*@\serverBox{1 + 2)}@*)
       
       for (int j = 5; (--j) >= 0;) {  //process only 5 clients, so I can show (*@\serverBox{5)}@*) below
         client = server.accept(); //wait for incoming connection (*@\serverBox{3)}@*)
+        
         System.out.println("New connection from " + client.getRemoteSocketAddress());
         
-        is     = client.getInputStream(); //get stream to read from
-        System.out.println(is.read());//(*@\serverBox{4} + \clientBox{3})@*)
+        dis     = new DataInputStream(client.getInputStream()); //get stream to read from
+        n = dis.read();//(*@\serverBox{4} + \clientBox{3})@*)
+        
+        for(int i = 0; i < n; i++){
+          System.out.print("X");
+        }
+         System.out.print('\n\n');
              
         client.close(); //close connection to client
       }
